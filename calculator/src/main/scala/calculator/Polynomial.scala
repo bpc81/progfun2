9ab7647c34a03d6cbcc7172b0a5b112176e2e04a
Ben Cook <cookbp@gmail.com>
2017-04-06 20:39:29 -0400

    implemented Polynomial.computeSolutions

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -8,6 +8,15 @@ object Polynomial {
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    def recompute(a: Double, b: Double, c: Double, delta: Double): Set[Double] = {
+      if (delta > 0d) {
+        val sqrtDelta = math.sqrt(delta)
+        Set(
+          (-b + sqrtDelta) / a / 2,
+          (-b - sqrtDelta) / a / 2)
+      } else if (delta == 0d) Set( -b / a / 2)
+      else Set[Double]()
+    }
+    Var[Set[Double]]( recompute(a(), b(), c(), delta()) )
   }
 }
